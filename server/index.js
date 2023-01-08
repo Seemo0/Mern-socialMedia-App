066cef7eb492f600f537ff92dc00308fb3392a77
Seemo0 <medouallal08@gmail.com>
2023-01-08 23:45:01 +0100

    frontend commit

--- server/index.js
+++ server/index.js
@@ -10,10 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts'
+import postRoutes from './routes/posts.js'
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
