70cf6130a7bba788c9bcb54607fa52c0d519fd29
Seemo0 <medouallal08@gmail.com>
2023-01-09 00:28:50 +0100

    frontend commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
-
   return (
-    <div className="App">
-      semo
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
